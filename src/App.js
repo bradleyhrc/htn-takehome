70604dd9b1a933bf475d057ff8e1cb8fbadee3d6
Bradley Herrera Contreras <bradleyhrct@gmail.com>
2023-02-20 01:11:07 -0500

    Implemented Fetch Retrieval

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from "react";
+
 
 function App() {
-  return (
+  // variables for pop-up modal
+  const [show, setShow] = useState(false);
+  const [eventInfo, setEventInfo] = useState(null); // tbd
+  // main data retrieval
+  const [eventData, setEventData] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  async function getData (url) {
+    await fetch(url)
+    .then(res => { // error handling
+      if (res.ok) return res.json();
+      throw res;
+    })
+    .then(data => { // 
+      console.log(data);
+      setEventData(data);
+    })
+    .catch(err => console.log("Error fetching data: ", err))
+    .finally(() => setLoading(false))
+  }
+
+  useEffect(() => { getData('https://api.hackthenorth.com/v3/events') }, []); // default value
+
+  const showModal = (event) => {
+    setEventInfo(event);
+    setShow(true);
+    console.log("Switched");
+  };
+
+  const exitModal = () => setShow(false);
+  
+  return ( 
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
+        {!loading && <p>{eventData[0].name}</p>}
+      </header>
+    </div>
+  );
+}
+
+/*<a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+        </a> */
 
 export default App;
